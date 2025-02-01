e8f0d500116dc4b7a3cf26b233d09713690b34c5
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-01 18:27:50 +0700

    .

--- app/Filament/Resources/ProdukResource.php
+++ app/Filament/Resources/ProdukResource.php
@@ -173,8 +173,9 @@ class ProdukResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                // Tables\Actions\EditAction::make(),
+                // Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
