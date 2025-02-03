32bdbe6d45352f9d5f9a6227831e2539c0f8919c
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-03 13:41:36 +0700

    .

--- resources/views/vendor/filament-tables/index.blade.php
+++ resources/views/vendor/filament-tables/index.blade.php
@@ -413,7 +413,7 @@
                         x-sortable
                         :data-sortable-animation-duration="$getReorderAnimationDuration()"
                         @class([
-                            'fi-ta-content-grid gap-3 px-3 sm:px-3 dedens' => $contentGrid,
+                            'fi-ta-content-grid gap-1 px-1 sm:px-1 dedens' => $contentGrid,
                             'pt-0' => $contentGrid && $this->getTableGrouping(),
                             'gap-y-px bg-gray-200 dark:bg-white/5' => ! $contentGrid,
                         ])
