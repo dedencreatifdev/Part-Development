80e69911dfee80601eaffdb7829edb2f3eb90d4c
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-04 09:42:12 +0700

    .

--- app/Filament/Widgets/ProdukListTable.php
+++ app/Filament/Widgets/ProdukListTable.php
@@ -3,6 +3,11 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Produk;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
@@ -14,13 +19,124 @@ use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
 
 class ProdukListTable extends BaseWidget
 {
     protected int | string | array $columnSpan = 'full';
     protected static ?int $sort = 3;
+
+    // use InteractsWithForms, InteractsWithViews, InteractsWithTable;
+
+    // public static function form(Form $form): Form
+    // {
+    //     return $form
+    //         ->schema([
+    //             FileUpload::make('image')
+    //                 ->columnSpanFull()
+    //                 ->image(),
+    //             TextInput::make('KDBR')
+    //                 ->label('Kode Barang')
+    //                 ->maxLength(100)
+    //                 ->default(null),
+    //             TextInput::make('NAMA')
+    //                 ->label('Nama Barang')
+    //                 ->maxLength(50)
+    //                 ->default(null),
+    //             TextInput::make('PRODUKSI')
+    //                 ->maxLength(50)
+    //                 ->default(null),
+    //             TextInput::make('KDGROUP')
+    //                 ->maxLength(50)
+    //                 ->default(null),
+    //             TextInput::make('KDLOKASI')
+    //                 ->maxLength(50)
+    //                 ->default(null),
+    //             TextInput::make('SATUAN')
+    //                 ->maxLength(50)
+    //                 ->default(null),
+    //             TextInput::make('HRG_BELI')
+    //                 ->numeric()
+    //                 ->default(null),
+    //             TextInput::make('HRG_JUAL')
+    //                 ->numeric()
+    //                 ->default(null),
+
+
+    //             // TextInput::make('HRG_HPP')
+    //             //     ->maxLength(50)
+    //             //     ->default(null),
+    //             // TextInput::make('DISC_BELI')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('DISC_JUAL')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('QTY_MIN')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('QTY_MAX')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('QTY_ORD')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('MVCD')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('DLCODE')
+    //             //     ->maxLength(50)
+    //             //     ->default(null),
+    //             // TextInput::make('KDNSB')
+    //             //     ->maxLength(50)
+    //             //     ->default(null),
+    //             // DatePicker::make('TG_PRICE'),
+    //             // Textarea::make('KETERANGAN')
+    //             //     ->columnSpanFull(),
+    //             // TextInput::make('KM_PAKAI')
+    //             //     ->maxLength(50)
+    //             //     ->default(null),
+    //             // TextInput::make('STATUS')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // DatePicker::make('TG_DAFTAR'),
+    //             // TextInput::make('ID_KODE')
+    //             //     ->maxLength(50)
+    //             //     ->default(null),
+    //             // TextInput::make('FLAG')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('FT_QTYOB')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('FT_QTYOJ')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('FT_QTYAKHIR')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('FT_HPPAKHIR')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('FT_HPPUNIT')
+    //             //     ->numeric()
+    //             //     ->default(null),
+    //             // TextInput::make('FT_NMGROUP')
+    //             //     ->maxLength(50)
+    //             //     ->default(null),
+    //             // TextInput::make('FT_LOKASI')
+    //             //     ->maxLength(50)
+    //             //     ->default(null),
+
+    //             // DateTimePicker::make('tgl_update')
+    //             //     ->required(),
+    //         ])
+    //         ->columns(4);
+    // }
 
     public function table(Table $table): Table
     {
@@ -91,11 +207,11 @@ class ProdukListTable extends BaseWidget
             ->actions([
                 // Tables\Actions\EditAction::make(),
                 // Tables\Actions\DeleteAction::make(),
-                Tables\Actions\ViewAction::make()
-                    ->size(ActionSize::ExtraSmall)
-                    ->label('Lihat Detail')
-                    ->icon('heroicon-o-clipboard-document-list')
-                    ->color('primary'),
+                // Tables\Actions\ViewAction::make()
+                //     ->size(ActionSize::ExtraSmall)
+                //     ->label('Lihat Detail')
+                //     ->icon('heroicon-o-clipboard-document-list')
+                //     ->color('primary'),
             ]);
     }
 
