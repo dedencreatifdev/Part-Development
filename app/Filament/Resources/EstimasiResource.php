2cfa3f1365d63ceb8c858c4b2c545a3636228ed6
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-08 15:59:49 +0700

    .

--- app/Filament/Resources/EstimasiResource.php
+++ app/Filament/Resources/EstimasiResource.php
@@ -41,7 +41,7 @@ class EstimasiResource extends Resource
             ->columns([
                 Stack::make([
                     Tables\Columns\ImageColumn::make('gambar')
-                        ->defaultImageUrl(fn() => url('storage/app/public/01JKJB4N8P659GMTBVADKSA3B2.jpg'))
+                        ->defaultImageUrl(fn(Estimasi $record) => url('storage/app/public/') . $record->image)
                         ->size('100%'),
                     Tables\Columns\TextColumn::make('nama_estimasi')
                         ->size(TextColumnSize::ExtraSmall)
