157d39a5f3433bb75db26854ceb7ae9ba0c255f9
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-11 08:21:24 +0700

    .

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -38,7 +38,7 @@ class UserResource extends Resource
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
                     ->password()
-                    ->required()
+                    // ->required()
                     ->maxLength(255),
                 Forms\Components\Select::make('roles')
                     ->relationship('roles', 'name')
